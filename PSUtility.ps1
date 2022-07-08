4c2670abedd55a477284610420a5cccfc0d395b1
Tahask11 <31264792+Tahask11@users.noreply.github.com>
2022-07-09 00:25:01 +0530

    Add Getmycredentials function

--- PSUtility.ps1
+++ PSUtility.ps1
@@ -29,3 +29,48 @@ function Write-Log {
         Severity = $Severity
     } | Export-Csv -Path $logpath -Append -NoTypeInformation
 }
+
+
+#=====================================================================
+# Get-MyCredential
+#=====================================================================
+function Get-MyCredential {
+    param(
+        $CredPath,
+        [switch]$Help
+    )
+    $HelpText = @"
+
+    Get-MyCredential
+    Usage:
+    Get-MyCredential -CredPath `$CredPath
+
+    If a credential is stored in $CredPath, it will be used.
+    If no credential is found, Export-Credential will start and offer to
+    Store a credential at the location specified.
+
+"@
+    if ($Help -or (!($CredPath))) {
+        write-host $Helptext; Break 
+    }
+    
+    if (!(Test-Path -Path $CredPath -PathType Leaf)) {
+        Export-Credential (Get-Credential) $CredPath
+    }
+
+    $cred = Import-Clixml $CredPath
+    $cred.Password = $cred.Password | ConvertTo-SecureString
+    $Credential = New-Object System.Management.Automation.PsCredential($cred.UserName, $cred.Password)
+    Return $Credential
+}
+
+#=====================================================================
+# Export-Credential
+# Usage: Export-Credential $CredentialObject $FileToSaveTo
+#=====================================================================
+function Export-Credential($cred, $path) {
+    $cred = $cred | Select-Object *
+    $cred.password = $cred.Password | ConvertFrom-SecureString
+    $cred | Export-Clixml $path
+}
+
